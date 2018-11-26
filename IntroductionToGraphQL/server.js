7f2afd70c07166868354ee0f1fd8181da5da3278
Leonardo Dias <leonardodias914@gmail.com>
2018-11-26 18:35:58 -0200

    express moved to api/app.js

---
+++ IntroductionToGraphQL/server.js
@@ -0,0 +1,7 @@
+const { port } = require('./api/config');
+const app = require('./api/app')
+
+app.listen(port, () => {
+    console.log('Connected');
+    console.log('Server started and running on port '+port)
+});
